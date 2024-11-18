4e7fae88d8bffdc723961c16a8e71e7cf31d5378
TopSGW <web.smartdev22@gmail.com>
2024-11-18 20:46:51 +0000

    update access token lifte time

--- doneez/settings.py
+++ doneez/settings.py
@@ -163,7 +163,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1260),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
